8028fd03109a64fbd2eb95aaa29525a8121ccd91
Ben Barnett <Ben@DN2lk5k46.stanford.edu>
2018-03-09 18:02:21 -0800

    Starting BiDAF

--- code/modules.py
+++ code/modules.py
@@ -175,6 +175,101 @@ class BasicAttn(object):
 
             return attn_dist, output
 
+class BiDAF(object):
+    """
+    Module for bidirectional attention flow.
+
+    Note: in this module we use the terminology of "keys" and "values" (see lectures).
+    In the terminology of "X attends to Y", "keys attend to values".
+
+    In the baseline model, the keys are the context hidden states
+    and the values are the question hidden states.
+
+    We choose to use general terminology of keys and values in this module
+    (rather than context and question) to avoid confusion if you reuse this
+    module with other inputs.
+    """
+
+    def __init__(self, keep_prob, key_vec_size, value_vec_size):
+        """
+        Inputs:
+          keep_prob: tensor containing a single scalar that is the keep probability (for dropout)
+          key_vec_size: size of the key vectors. int
+          value_vec_size: size of the value vectors. int
+        """
+        self.keep_prob = keep_prob
+        self.key_vec_size = key_vec_size
+        self.value_vec_size = value_vec_size
+
+    def build_graph(self, values, values_mask, keys):
+        """
+        Keys attend to values.
+        For each key, return an attention distribution and an attention output vector.
+
+        Inputs:
+          values: Tensor shape (batch_size, num_values, value_vec_size).
+          values_mask: Tensor shape (batch_size, num_values).
+            1s where there's real input, 0s where there's padding
+          keys: Tensor shape (batch_size, num_keys, value_vec_size)
+
+        Outputs:
+          attn_dist: Tensor shape (batch_size, num_keys, num_values).
+            For each key, the distribution should sum to 1,
+            and should be 0 in the value locations that correspond to padding.
+          output: Tensor shape (batch_size, num_keys, hidden_size).
+            This is the attention output; the weighted sum of the values
+            (using the attention distribution as weights).
+        """
+        with vs.variable_scope("BiDAF"):
+
+            # Calculate attention distribution
+            #values_t = tf.transpose(values, perm=[0, 2, 1]) # (batch_size, value_vec_size, num_values)
+            w1 = tf.get_variable('w1', shape=(self.value_vec_size, 1), initializer=tf.contrib.layers.xavier_initializer())
+            w2 = tf.get_variable('w2', shape=[self.value_vec_size, 1], initializer=tf.contrib.layers.xavier_initializer())
+            w3 = tf.get_variable('w3', shape=[self.value_vec_size, 1], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+
+            num_keys = keys.get_shape()[1]
+            num_vals = values.get_shape()[1]
+
+            c = tf.reshape(keys, shape=[-1, self.value_vec_size])
+            q = tf.reshape(keys, shape=[-1, num_vals, self.value_vec_size])
+
+            first = tf.reshape(tf.matmul(c, w1), shape=[-1, num_keys]) # (m, N)
+            print "first"
+            print first
+            first_component = tf.tile(first, num_vals) # (m, N, M)
+            print "first component"
+            print first_component
+            second = tf.matmul(vals, w2) # (m, M)
+            print "second"
+            print second
+            second_component = tf.transpose(tf.tile(second, num_keys), perm=[0, 2, 1]) # (m, N, M)
+            print "second component"
+            print second_component
+            third = keys * w3 # (m, N, value_vec_size)
+            print "third"
+            print third
+            third_component = tf.matmul(third, tf.transpose(values)) # (m, N, M) TODO I HOPE?
+            print "third component"
+            print third_component
+            similarity_matrix = first_component + second_component + third_component # (m, N, M)
+
+            attn_logits = tf.matmul(keys, values_t) # shape (batch_size, num_keys, num_values)
+            attn_logits_mask = tf.expand_dims(values_mask, 1) # shape (batch_size, 1, num_values)
+            _, attn_dist = masked_softmax(attn_logits, attn_logits_mask, 2) # shape (batch_size, num_keys, num_values). take softmax over values
+
+            # Use attention distribution to take weighted sum of values
+            output = tf.matmul(attn_dist, values) # shape (batch_size, num_keys, value_vec_size)
+
+
+
+
+
+
+            # Apply dropout
+            output = tf.nn.dropout(output, self.keep_prob)
+
+            return attn_dist, output
 
 def masked_softmax(logits, mask, dim):
     """
